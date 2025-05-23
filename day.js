daec06ff41a0939e9d3298dd4eef2563425a6bf8
virender yadav <virendaryadav831@gmail.com>
2025-05-23 12:55:30 +0530

    change in column

--- day.js
+++ day.js
@@ -1,4 +1,8 @@
-function sum(a,b){
-    return (a+b);
+let i,j;
+//const n = parseInt(prompt("enter your value"));
+for(i=1;i<=4;i++){
+    for(j=1;j<=4;j++){
+        console.log("*");
+    }
+  //  console.log();
 }
-console.log(sum(4,6));
